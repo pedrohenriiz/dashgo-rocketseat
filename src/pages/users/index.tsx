60e71f7f4959fb78da473c6a27af17a872a34cd4
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2022-07-23 16:12:51 -0300

    Adds user data on table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,25 +13,50 @@ import {
   Tr,
   Text,
   useBreakpointValue,
+  Spinner,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    ['users'],
+    async () => {
+      const response = await fetch('http://localhost:8000/api/users');
+      const data = await response.json();
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
+
+      return users;
+    },
+    { staleTime: 1000 * 5 }
+  );
+
+  console.log(data);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
-  useEffect(() => {
-    fetch('http://localhost:8000/api/users')
-      .then((response) => response.json())
-      .then((response) => console.log(response));
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <Box>
@@ -59,52 +84,70 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink'></Checkbox>
-                </Th>
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink'></Checkbox>
+                    </Th>
 
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                {isWideVersion && <Th w='8'>Ações</Th>}
-              </Tr>
-            </Thead>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    {isWideVersion && <Th w='8'>Ações</Th>}
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink'></Checkbox>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Pedro henrique</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      pedro.avare01@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
+                <Tbody>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink'></Checkbox>
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Text fontSize='small' color='gray.300'>
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
 
-                {isWideVersion && <Td>16 de Julho, 2022</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as='a'
-                      size='sm'
-                      fontSize='small'
-                      colorScheme='purple'
-                      leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                    >
-                      {isWideVersion ? 'Editar' : ''}
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        {isWideVersion && (
+                          <Td>
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='small'
+                              colorScheme='purple'
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize='16' />
+                              }
+                            >
+                              {isWideVersion ? 'Editar' : ''}
+                            </Button>
+                          </Td>
+                        )}
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
